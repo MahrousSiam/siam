19abb875aee0287a612470a6c35667f8e44ddf45
MahrousSiam <68647171+MahrousSiam@users.noreply.github.com>
2023-09-12 15:04:49 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,29 +1,17 @@
 ####################################
 ####################################
-from time import sleep as timeout
-import time
-import base64
-import socket
-import sys
 import os
-#try:
+
 os.system('clear')
 ####################################
-def restart () :
-    os.system('clear')
-    print (' ')
-    bnn ()
-    print (' ')
-    print (' ')
-    print (' ')
-    print (' ')
-    print (' ')
-    print (' ')
-    print ("\033[1;31m                 ╔╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╗")
-    print ("\033[1;31m                 ╢                         ╢")
-    print ("\033[1;31m                 ╢\033[1;33m    Back To Main ...?   \033[1;31m ╢")
-    print ("\033[1;31m                 ╢                         ╢")
-    print ("\033[1;31m                 ╚╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╝")
-    print (' ')
-    print (' ')
-    
+print (' ')
+print (' ')
+print (' ')
+print ("\033[1;31m                 ╔╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╗")
+print ("\033[1;31m                 ╢                         ╢")
+print ("\033[1;31m                 ╢\033[1;33m    Back To Main ...?   \033[1;31m ╢")
+print ("\033[1;31m                 ╢                         ╢")
+print ("\033[1;31m                 ╚╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╝")
+print (' ')
+print (' ')
+print (' ')
